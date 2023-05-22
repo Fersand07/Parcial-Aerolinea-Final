b5d754d16c3b4116346d85c8ca9b8912866b1c24
Fersand07 <00007821@uca.edu.sv>
2023-05-22 13:55:52 -0600

    Model

---
+++ app/src/main/java/com/example/parcial_aerolinea/data/DummyData.kt
@@ -0,0 +1,10 @@
+package com.example.parcial_aerolinea.data
+
+import com.example.parcial_aerolinea.data.model.AirlineModel
+
+val name = "Avianca"
+val country = "El Salvador"
+
+var airlines = mutableListOf(
+    AirlineModel(name, country)
+)
