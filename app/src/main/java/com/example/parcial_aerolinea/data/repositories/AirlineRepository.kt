7cb49df142bff3920c2044a77e3803f0fb5a6a32
Fersand07 <00007821@uca.edu.sv>
2023-05-22 14:06:19 -0600

    Respository

---
+++ app/src/main/java/com/example/parcial_aerolinea/data/repositories/AirlineRepository.kt
@@ -0,0 +1,11 @@
+package com.example.parcial_aerolinea.data.repositories
+
+import com.example.parcial_aerolinea.data.model.AirlineModel
+
+class AirlineRepository(private val airlines: MutableList<AirlineModel>) {
+
+    fun getAirlines() = airlines
+
+    fun addAirlines(airline: AirlineModel) = airlines.add(airline)
+
+}
